f3aec461ef37bae4283daa8801467286a5dea503
sjovanovic974 <sjovanovic974@gmail.com>
2021-11-17 15:48:29 +0100

    Initial Commit (Setting up the project)

---
+++ src/main/java/sasa/jovanovic/thymeleafdemo/ThymeleafDemoEmployeeListApplication.java
@@ -0,0 +1,13 @@
+package sasa.jovanovic.thymeleafdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThymeleafDemoEmployeeListApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ThymeleafDemoEmployeeListApplication.class, args);
+	}
+
+}
---
+++ src/test/java/sasa/jovanovic/thymeleafdemo/ThymeleafDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package sasa.jovanovic.thymeleafdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ThymeleafDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
